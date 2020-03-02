5d5849bb015bbcd3e9701112501d4dd60fa8d1cb
YilberG <yilber-99@hotmail.com>
2020-03-02 18:58:05 -0500

    buscar palabra

---
+++ Palabra.php
@@ -0,0 +1,36 @@
+
+<html>
+<h4>
+<body>
+    
+    <br>
+    <?php
+    $arreglo = ["baseball","cat,bas,hol,base,rar,pull,ball"];
+    $salida = explode(", ",$arreglo[1]);
+    for($i=0; $i < count($salida);$i++)
+    {
+        echo $salida[$i];
+        echo "<br>";
+    }
+    $erre="";
+
+    for($a=0;$a<count($salida);$a++)
+    {
+        for($b=0;$b<count($salida);$b++)
+        {
+            if($a!=$b)
+            {
+                $erre = $salida[$a].$salida[$b];
+                if($erre == $arreglo[0])
+                {
+                    echo "salida valor: ".$salida[$a].", ".$salida[$b];
+                }
+            }
+        }
+    }
+
+    ?>
+    <br>
+   
+    </h4>
+</body>
